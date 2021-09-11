4f4558ac8e3011080f5a530e421d5fb9849dda95
Mr. Robot <t0thkr1s@icloud.com>
2021-09-11 23:46:43 +0200

    Create GlassHouse.kt

---
+++ Behavioral Patterns/Template/GlassHouse.kt
@@ -0,0 +1,10 @@
+class GlassHouse : House() {
+
+    override fun buildWalls() {
+        println("Building glass walls.")
+    }
+
+    override fun buildPillars() {
+        println("Building glass pillars.")
+    }
+}
