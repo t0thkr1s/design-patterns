d10e0c721bccef7d2adbf3e49229a527750d6967
Mr. Robot <t0thkr1s@icloud.com>
2021-09-11 23:28:55 +0200

    Create WoodenHouse.kt

---
+++ Behavioral Patterns/Template/WoodenHouse.kt
@@ -0,0 +1,14 @@
+class WoodenHouse : House() {
+
+    override fun buildWalls() {
+        println("Building wooden walls.")
+    }
+
+    override fun buildPillars() {
+        println("Building wooden pillars.")
+    }
+
+    override fun buildCustomDoor() {
+        println("Building custom door.")
+    }
+}
